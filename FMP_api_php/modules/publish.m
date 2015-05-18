bfe11fb9395f64faea0eee23ebc1761fab79da7f
evoup <evoex123@gmail.com>
2015-05-18 14:54:31 +0800

    c

--- FMP_api_php/modules/publish.m
+++ FMP_api_php/modules/publish.m
@@ -68,6 +68,7 @@ case (__OBJT_MULTI_PRODUCT):
         $image[$i]->create();
         echo $image[$i]->hash;
         unlink($tmpFile);
+        // Create Multi Product Ad Creative
         $child_attachments[$i][AttachmentDataFields::NAME]=$multiProductsInfo[$i]['product_name'];
         $child_attachments[$i][AttachmentDataFields::DESCRIPTION]=$multiProductsInfo[$i]['product_desc'];
         $child_attachments[$i][AttachmentDataFields::LINK]=$multiProductsInfo[$i]['product_link'];
@@ -75,49 +76,19 @@ case (__OBJT_MULTI_PRODUCT):
     }
     break;
 }
-echo "child_attachments:";
-print_r($child_attachments);
-//die;
-//foreach($_POST['commit_data'] as $cdInfo) {
-    //print_r($cdInfo);
-//}
-
-// Upload Images
-//echo "Uploading Images";
-
-//for($i = 1; $i <= 3; $i++) {
-  //$image[$i] = new AdImage(null, $account_id);
-  //$image[$i]->{AdImageFields::FILENAME} = "/tmp/africa/africa{$i}.jpg";
-  //echo ".";
-  //$image[$i]->create();
-//}
-
-//echo "\n";
-
-// Create Multi Product Ad Creative
-
-//$child_attachments = array();
-
-//for($i = 1; $i <= 3; $i++) {
-  //$child_attachments[] = array(
-    //AttachmentDataFields::NAME => 'product'.$i,
-    //AttachmentDataFields::DESCRIPTION => '$'.$i.'00',
-    //AttachmentDataFields::LINK => 'http://photos.vigyaan.com/africa'.$i,
-    //AttachmentDataFields::IMAGE_HASH => $image[$i]->hash
-  //);
-//}
 
 // The ObjectStorySpec helps bring some order to a complex
 // API spec that assists with creating page posts inline.
 
-$object_story_spec = array(
+@$object_story_spec = array(
   ObjectStorySpecFields::PAGE_ID => $page_id,
   ObjectStorySpecFields::LINK_DATA => array(
-    //LinkDataFields::MESSAGE => 'Check out some pictures from Africa',
-    LinkDataFields::LINK => 'http://photos.vigyaan.com',
-    LinkDataFields::CAPTION => 'http://photos.vigyaan.com',
+    LinkDataFields::MESSAGE => $_SESSION[__SESSION_CAMP_EDIT]['step5']['message'],
+    LinkDataFields::LINK => $_SESSION[__SESSION_CAMP_EDIT]['step5']['link'],
+    LinkDataFields::CAPTION => $_SESSION[__SESSION_CAMP_EDIT]['step5']['link'],
     LinkDataFields::CHILD_ATTACHMENTS => $child_attachments
 ));
+
 
 $creative = new AdCreative(null, $account_id);
 
@@ -130,27 +101,6 @@ $creative->create();
 $creative_id = $creative->id;
 echo 'Creative ID: '.$creative_id ."\n";
 
-// Create Audience
-// Learn more about custom audiences here
-// https://developers.facebook.com/docs/marketing-api/custom-audience-website
-
-//$audience = new CustomAudience(null, $account_id);
-//$audience->setData(array(
-  //CustomAudienceFields::NAME => 'f8 demo custom audience',
-  //CustomAudienceFields::DESCRIPTION => 'people who visited our website',
-  //CustomAudienceFields::SUBTYPE => 'WEBSITE',
-  //CustomAudienceFields::RETENTION_DAYS => 30,
-  //CustomAudienceFields::PREFILL => true,
-  //CustomAudienceFields::RULE => array(
-    //'url' => array(
-      //'i_contains' => ''
-    //)),
-  //'pixel_id' => $pixel_id,
-//));
-
-//$audience->create();
-//$audience_id = $audience->id;
-//echo "Audience ID: " . $audience_id."\n";
 
 // Create a campaign
 // A campaign can have one or more adsets
@@ -158,7 +108,6 @@ echo 'Creative ID: '.$creative_id ."\n";
 $campaign  = new AdCampaign(null, $account_id);
 @$campaign->setData(array(
   AdCampaignFields::NAME => $_SESSION[__SESSION_CAMP_EDIT] ['step1']['campaignName'],
-  //AdCampaignFields::OBJECTIVE => AdObjectives::WEBSITE_CONVERSIONS,
   AdCampaignFields::OBJECTIVE => AdObjectives::WEBSITE_CLICKS,
   AdCampaignFields::STATUS => AdCampaign::STATUS_PAUSED
 ));
@@ -167,62 +116,50 @@ $campaign->validate()->create();
 $campaign_id = $campaign->id;
 echo "Campaign ID: " . $campaign_id . "\n";
 
-// Create targeting
+echo "----------------------------\n";
+foreach($_POST['commit_data'] as $cdInfo) {
+    print_r($cdInfo);
+    /*{{{*/
+    // Create targeting
+    $targeting = new TargetingSpecs();
+    $targeting->{TargetingSpecsFields::GEO_LOCATIONS}
+    = array(
+        'countries' => explode('|', $cdInfo['location'])
+    );
 
-$targeting = new TargetingSpecs();
-$targeting->{TargetingSpecsFields::GEO_LOCATIONS}
-= array(
-    'countries' => explode('|',$_SESSION[__SESSION_CAMP_EDIT]['step3']['location'])
-);
-//$targeting->{TargetingSpecsFields::CUSTOM_AUDIENCES} = array(
-  //'id' => $audience_id);
+    // Create an ad set
+    // An adset can have one or more ad units
+    $adset = new AdSet(null, $account_id);
+    @$adset->setData(array(
+        AdSetFields::NAME => $cdInfo['ad_set_name'],
+        AdSetFields::CAMPAIGN_GROUP_ID => $campaign_id,
+        AdSetFields::CAMPAIGN_STATUS => AdSet::STATUS_ACTIVE,
+        AdSetFields::DAILY_BUDGET => $_SESSION[__SESSION_CAMP_EDIT]['step4']['budget']*100,
+        AdSetFields::TARGETING => $targeting,
+        AdSetFields::BID_TYPE => BidTypes::BID_TYPE_CPM,
+        AdSetFields::BID_INFO => array(AdGroupBidInfoFields::IMPRESSIONS => 2),
+        AdSetFields::PROMOTED_OBJECT => array('page_id' => $page_id),
+    ));
 
-// Create an ad set
-// An adset can have one or more ad units
+    $adset->validate()->create();
+    $adset_id = $adset->id;
+    echo 'AdSet ID: '. $adset_id . "\n";
 
-$adset = new AdSet(null, $account_id);
-@$adset->setData(array(
-  AdSetFields::NAME => 'My First AdSet',
-  AdSetFields::CAMPAIGN_GROUP_ID => $campaign_id,
-  AdSetFields::CAMPAIGN_STATUS => AdSet::STATUS_ACTIVE,
-  AdSetFields::DAILY_BUDGET => $_SESSION[__SESSION_CAMP_EDIT]['step4']['budget']*100,
-  AdSetFields::TARGETING => $targeting,
-  AdSetFields::BID_TYPE => BidTypes::BID_TYPE_CPM,
-  AdSetFields::BID_INFO => array(AdGroupBidInfoFields::IMPRESSIONS => 2),
-  //AdSetFields::BID_TYPE => BidTypes::BID_TYPE_ABSOLUTE_OCPM,
-  //AdSetFields::BID_INFO => array('ACTIONS' => 3000),
-  AdSetFields::PROMOTED_OBJECT => array('page_id' => $page_id),
-  //AdSetFields::PROMOTED_OBJECT => array('pixel_id' => $pixel_id),
-));
+    // Create an ad
+    // Each ad can only have one creative
+    echo "account_id:{$account_id}";
+    echo "adset_id:{$adset_id}";
+    echo "creative_id:{$creative_id}";
+    $adgroup = new AdGroup(null, $account_id);
+    $adgroup->{AdGroupFields::ADGROUP_STATUS} = AdGroup::STATUS_ACTIVE;
+    //$adgroup->{AdGroupFields::NAME} = 'test adgroup';
+    $adgroup->{AdGroupFields::NAME} = $cdInfo['ad_set_name'];
+    $adgroup->{AdGroupFields::CAMPAIGN_ID}=$adset_id;
+    $adgroup->{AdGroupFields::CREATIVE} = array('creative_id' => $creative_id);
 
-$adset->validate()->create();
-$adset_id = $adset->id;
-echo 'AdSet ID: '. $adset_id . "\n";
+    $adgroup->create();
+    $adgroup_id=$adgroup->id;
+    echo 'AdGroup ID: ' . $adgroup_id . "\n";
+    /*}}}*/
+}
 
-// Create an ad
-// Each ad can only have one creative
-
-//$adgroup = new AdGroup(null, $account_id);
-//$adgroup->setData(array(
-  //AdGroupFields::CREATIVE => array('creative_id' => $creative_id),
-  //AdGroupFields::NAME => 'My First AdGroup',
-  //AdGroupFields::CAMPAIGN_ID => $adset_id,
-  //AdGroupFields::ADGROUP_STATUS => 'ACTIVE'
-//));
-
-
-echo "account_id:{$account_id}";
-echo "adset_id:{$adset_id}";
-echo "creative_id:{$creative_id}";
-$adgroup = new AdGroup(null, $account_id);
-$adgroup->{AdGroupFields::ADGROUP_STATUS} = AdGroup::STATUS_PAUSED;
-$adgroup->{AdGroupFields::NAME} = 'test adgroup';
-$adgroup->{AdGroupFields::CAMPAIGN_ID}=$adset_id;
-$adgroup->{AdGroupFields::CREATIVE} = array('creative_id' => $creative_id);
-
-
-
-
-$adgroup->create();
-$adgroup_id=$adgroup->id;
-echo 'AdGroup ID: ' . $adgroup_id . "\n";
