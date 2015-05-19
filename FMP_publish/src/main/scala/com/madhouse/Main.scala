38e85136c5db4be4d6f5a4de31273bac4aeaa2b4
evoup <evoex123@gmail.com>
2015-05-19 17:11:10 +0800

    连接mysql

--- FMP_publish/src/main/scala/com/madhouse/Main.scala
+++ FMP_publish/src/main/scala/com/madhouse/Main.scala
@@ -1,7 +1,33 @@
 package com.madhouse
+import java.sql.DriverManager
+import java.sql.Connection
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+    def main(args: Array[String]): Unit = {
+        // connect to the database named "mysql" on the localhost
+        val driver = "com.mysql.jdbc.Driver"
+        val url = "jdbc:mysql://localhost/fmp"
+        val username = "madcore"
+        val password = "madcore"
+        // there's probably a better way to do this
+        var connection:Connection = null
+        try {
+            // make the connection
+            Class.forName(driver)
+            connection = DriverManager.getConnection(url, username, password)
+
+            // create the statement, and run the select query
+            val statement = connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT id, email FROM t_fmp_user")
+            while ( resultSet.next() ) {
+                val id = resultSet.getString("id")
+                val email = resultSet.getString("email")
+                println("id, email = " + id + ", " + email)
+            }
+        } catch {
+            case e => e.printStackTrace
+        }
+        connection.close()
+        println("Hello, world!")
+    }
 }
