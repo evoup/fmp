901f06857ee6a1b2ece1191ce028558e430aaf48
CentOS65_pmd <evoup@localhost.localdomain>
2015-06-24 18:35:18 +0800

    commit with bug

--- FMP_api_java/src/main/java/com/madhouse/fmp/config/SocialConfig.java
+++ FMP_api_java/src/main/java/com/madhouse/fmp/config/SocialConfig.java
@@ -1,12 +1,21 @@
 package com.madhouse.fmp.config;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
-
 
 /**
  * Spring Social Configuration.
@@ -18,11 +27,32 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 @EnableSocial
 
 public class SocialConfig implements SocialConfigurer{
+	@Inject
+	private DataSource dataSource;
 	//
 	// SocialConfigurer implementation methods
 	//
 	@Override
 	public void addConnectionFactories(ConnectionFactoryConfigurer cfConfig, Environment env) {
 		cfConfig.addConnectionFactory(new FacebookConnectionFactory(env.getProperty("facebook.appKey"), env.getProperty("facebook.appSecret")));
+	}
+	
+	@Override
+	public UserIdSource getUserIdSource() {
+		return new UserIdSource() {			
+			@Override
+			public String getUserId() {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if (authentication == null) {
+					throw new IllegalStateException("Unable to get a ConnectionRepository: no user signed in");
+				}
+				return authentication.getName();
+			}
+		};
+	}
+	
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
 	}
 }
