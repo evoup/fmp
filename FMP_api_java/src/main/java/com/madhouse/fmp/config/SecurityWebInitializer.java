d92ffd4dd83546715930f0e507fe668a548a3495
evoup <evoex123@gmail.com>
2015-06-29 15:14:27 +0800

    delete none used project

--- FMP_api_java/src/main/java/com/madhouse/fmp/config/SecurityWebInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.madhouse.fmp.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
--- FMP_api_java/src/main/java/com/madhouse/fmp/dao/FbAccountDAO.java
+++
@@ -1,9 +0,0 @@
-package com.madhouse.fmp.dao;
-
-import com.madhouse.fmp.model.FbAccount;
-
-public interface FbAccountDAO 
-{
-	public void saveOrUpdate(FbAccount fbaccount,int type);
-	
-}
